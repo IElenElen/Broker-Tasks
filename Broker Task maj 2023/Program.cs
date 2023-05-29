14bea9f0e97864338ee1b07db39c32208816fc74
Ilka <Ilka@LAPTOP-VAVOB6QC>
2023-05-29 18:58:53 +0200

    Add project files.

---
+++ Broker Task maj 2023/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Broker_Task_maj_2023
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Próby");
+        }
+    }
+}
